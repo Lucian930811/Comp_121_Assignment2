1a893558725d413fcaccaa63de1e2ae3be18a770
Lucian Lu <hsingl1@uci.edu>
2024-04-23 21:30:34 -0700

    scraper.py first draft

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,22 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    all_links = []
+
+    if resp.status == 200:
+
+        parsedHTML = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+        for anchor_tag in parsedHTML.find_all('a', href = True):
+
+            the_link = anchor_tag['href']
+
+            joined_link = urljoin(url, the_link)
+
+            all_links.append(joined_link)
+        
+
+    return all_links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -25,15 +41,29 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        if any(parsed.path.lower().endswith(ext) for ext in ['.pdf', '.doc', '.zip']):
+            return False
+        
+        if parsed.netloc in [
+            "www.ics.uci.edu",
+            "www.cs.uci.edu",
+            "www.informatics.uci.edu",
+            "www.stat.uci.edu"
+        ]:
+            return True
+        
+        return False
+
+
+        # return not re.match(
+        #     r".*\.(css|js|bmp|gif|jpe?g|ico"
+        #     + r"|png|tiff?|mid|mp2|mp3|mp4"
+        #     + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+        #     + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+        #     + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+        #     + r"|epub|dll|cnf|tgz|sha1"
+        #     + r"|thmx|mso|arff|rtf|jar|csv"
+        #     + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
